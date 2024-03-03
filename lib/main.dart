500a8a5115415706bb893d0a4d29062dee78448e
graduation <hastyaroumar1@gmail.com>
2024-03-03 09:19:15 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'shanya',
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
