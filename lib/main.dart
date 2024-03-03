ccdffaad24b5054264a9fb2418d29b0080255227
Your Name <hastyaroumar1@gmail.com>
2024-03-03 22:56:56 +0300

    d

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,8 @@
 // // //  //await Firebase.initializeApp();
 // // runApp(MyApp());}
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:graduation_app/firebase_options.dart';
 
 //import 'package:firebase_core/firebase_core.dart';
 
@@ -28,7 +26,7 @@ void main() {
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-  _initializeFirebase();
+
 //   await Firebase.initializeApp(
 //     options: DefaultFirebaseOptions.currentPlatform,
 // );
@@ -67,11 +65,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-_initializeFirebase() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-}
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
