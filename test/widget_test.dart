815423c7f7b4a4522075b16d7eca413f22541c21
graduation <hastyaroumar1@gmail.com>
2024-03-04 12:24:16 +0300

    c

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:graduation_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidget('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
